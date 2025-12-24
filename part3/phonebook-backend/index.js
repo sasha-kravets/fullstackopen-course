303568ea351dd49f8ed0632e8aa781c7748d0e43
sasha-kravets <skravets.home@gmail.com>
2025-12-24 19:04:33 +0200

    Completed exercises 3.9.-3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('person', (req) => {
@@ -100,7 +101,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
