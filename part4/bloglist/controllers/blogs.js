06ece642b9d389b6f2eb38cd788f4e81ce7476ad
sasha-kravets <skravets.home@gmail.com>
2026-02-15 16:45:31 +0200

    Completed exercises 4.1-4.2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => {
+      logger.error(error.message)
+      response.status(400).json({ error: 'malformatted or missing data' })
+    })
+})
+
+module.exports = blogsRouter
